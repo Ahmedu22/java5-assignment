dbd413ae557c2eeadd4bb1c25e21edabaf887455
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-16 09:55:07 +0300

    java5-assignment

---
+++ app.js
@@ -0,0 +1,42 @@
+// question one
+
+let number = 24;
+if(number < 24){
+    console.log('the number is positive')
+}
+else if(number >= 24){
+    console.log('the number is negative')
+}
+
+// question two
+
+let age = 18;
+if(age < 18){
+    console.log('you are un eligible to vote')
+}
+else if (age >= 18){
+    console.log('you are eligible to vote')
+}
+
+// question three
+
+let num = 17;
+if(num === 17){
+    console.log('the number is an even number')
+}
+else if(num < 17){
+    console.log('the number an odd number')
+}
+
+// question four
+
+let marks = 90;
+if (marks < 90){
+    console.log('grade C')
+}
+else if (marks === 90 ){
+    console.log('grade A')
+}
+
+// question five
+
